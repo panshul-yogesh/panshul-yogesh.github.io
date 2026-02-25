2a05986afab84097d14ced513f42709ad8dc6a57
pyogesh <pyogesh@opentext.com>
2026-02-25 08:45:17 +0530

    empty

--- chunk-CKSXNIT5.js
+++
@@ -1,1 +0,0 @@
-import{Ib as a,aa as r,ga as e}from"./chunk-XSDQJDJQ.js";var m=class t{constructor(i){this.sanitizer=i}transform(i){return this.sanitizer.bypassSecurityTrustHtml(i)}static \u0275fac=function(n){return new(n||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{m as a};
